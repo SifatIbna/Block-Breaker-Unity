953f5b09544396b4d7abc42ff869a579f317d5e4
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2021-04-28 04:41:10 +0600

    block adding, destroying, playing sound complete

---
+++ Assets/Scripts/LooseCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LooseCollider : MonoBehaviour
+{
+    //[SerializeField] private SceneLoader sceneLoader;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //sceneLoader.LoadNextScene();
+        SceneManager.LoadScene("Game Over");
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    private float positionX;
+    private float positionY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionX = 0;
+        positionY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        float mouseX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        //if (mouseX <0)
+        //{
+        //    mouseX = 0;
+        //}
+        //else if (mouseX > 16)
+        //{
+        //    mouseX = 16;
+        //}
+
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mouseX, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
